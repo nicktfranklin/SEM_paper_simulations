a43f6e4c9ac85ed15ec77b65363207dcec0372a6
nickfranklin <nthompsonfranklin@gmail.com>
2017-09-28 14:27:38 -0400

    added better implementation of edward based-event model, using a tensorflow slim and a MAP approximation

--- sem.py
+++ sem.py
@@ -6,7 +6,7 @@ from scipy.misc import logsumexp
 from keras.models import Sequential
 from keras.layers import Dense, Activation, SimpleRNN
 from tqdm import tnrange
-from tensorflow.contrib import slim
+from tensorflow.contrib import slim, rnn
 
 
 class EventModel(object):
@@ -103,7 +103,7 @@ class KerasSRN(KerasLDS):
         self.model.fit(self.x_train, self.y_train, verbose=0)
 
 
-class EdwardLinearDynamicSystem(EventModel):
+class EdwardLDS(EventModel):
     def __init__(self, D, n_samples=100):
         """
         Parameters:
@@ -262,8 +262,8 @@ class EdwardLinearDynamicSystem(EventModel):
         return np.reshape(Y_hat, newshape=(n, 1, self.D))
 
 
-class EdwardNN(EventModel):
-    def __init__(self, D, n_samples=100):
+class EdwardModel(EventModel):
+    def __init__(self, D):
         """
         Parameters:
         -----------
@@ -280,9 +280,6 @@ class EdwardNN(EventModel):
         self.y_train = np.zeros((0, self.D))
 
         # initialize samples, for untrained (new) models, predict X'_hat = X
-        self.n_samples = n_samples
-        self.w_samples = np.reshape(np.eye(self.D), (1, self.D, self.D))
-        self.b_samples = np.zeros((1, self.D))
         self.isInitialized = False
 
     def _estimate(self, x_train, y_train):
@@ -393,10 +390,9 @@ class EdwardNN(EventModel):
             N, D = np.shape(X)
 
         if self.isInitialized:
-            n = self.w_samples.shape[0]  # number of samples stored
             Y_hat = self.sess.run(self.Y, feed_dict={self.X_ph: np.reshape(X, newshape=(N, D))})
 
-            return np.reshape(Y_hat, newshape=(n, 1, self.D))
+            return np.reshape(Y_hat, newshape=(N, 1, self.D))
         else:
             return X
 
@@ -407,6 +403,192 @@ class EdwardNN(EventModel):
         """
         self.sess.close()
 
+
+class EdwardNN(EdwardModel):
+
+    def _estimate(self, x_train, y_train):
+        """
+        Parameters
+        ----------
+
+        x_train: NxD array
+            N exemplars of D-dimensional states
+
+        y_train: NxD array
+            N exemplars of D-dimensional successor states
+
+        n_samples: int
+            number of samples to use in inference
+        """
+        N, D = x_train.shape
+
+        # Define the neural network
+        self.X_ph = tf.placeholder(tf.float32, [None, D])
+
+        def neural_network(X):
+            """ Neural network with one hidden layer, outputs location and scale parameter
+            for the event model:
+            mean, location = neural_network(X)
+            """
+            hidden = slim.fully_connected(X, D * D, activation_fn=None)
+            loc = slim.fully_connected(hidden, D, activation_fn=None)
+            scale = slim.fully_connected(hidden, D, activation_fn=tf.nn.softplus)
+            return loc, scale
+
+        loc, scale = neural_network(self.X_ph)
+        self.Y = ed.models.MultivariateNormalDiag(loc=loc, scale_diag=scale)
+
+        # inference = ed.KLqp(data={self.X_ph: x_train, self.Y: y_train})  # variational inference
+        inference = ed.MAP(data={self.X_ph: x_train, self.Y: y_train})  # MAP approximation
+        inference.run()
+
+        self.sess = ed.get_session()
+
+
+class EdwardNN0(EdwardModel):
+
+    def _estimate(self, x_train, y_train):
+        """
+        Parameters
+        ----------
+
+        x_train: NxD array
+            N exemplars of D-dimensional states
+
+        y_train: NxD array
+            N exemplars of D-dimensional successor states
+
+        n_samples: int
+            number of samples to use in inference
+        """
+        N, D = x_train.shape
+
+        # Define the neural network
+        self.X_ph = tf.placeholder(tf.float32, [None, D])
+
+        def neural_network(X):
+            """ Neural network with one hidden layer, outputs location and scale parameter
+            for the event model:
+            mean, location = neural_network(X)
+            """
+            loc = slim.fully_connected(X, D, activation_fn=None)
+            scale = slim.fully_connected(X, D, activation_fn=tf.nn.softplus)
+            return loc, scale
+
+        loc, scale = neural_network(self.X_ph)
+        self.Y = ed.models.MultivariateNormalDiag(loc=loc, scale_diag=scale)
+
+        # inference = ed.KLqp(data={self.X_ph: x_train, self.Y: y_train})  # variational inference
+        inference = ed.MAP(data={self.X_ph: x_train, self.Y: y_train})  # MAP approximation
+        inference.run()
+
+        self.sess = ed.get_session()
+
+
+class EdwardRNN(EdwardModel):
+
+    def _estimate(self, x_train, y_train):
+        """
+        Parameters
+        ----------
+
+        x_train: NxD array
+            N exemplars of D-dimensional states
+
+        y_train: NxD array
+            N exemplars of D-dimensional successor states
+
+        n_samples: int
+            number of samples to use in inference
+        """
+        N, D = x_train.shape
+
+        # Define the neural network
+        self.X_ph = tf.placeholder(tf.float32, [None, D])
+
+        def neural_network(X):
+            """ Neural network with one hidden layer, outputs location and scale parameter
+            for the event model:
+            mean, location = neural_network(X)
+            """
+            h = rnn.BasicRNNCell(X, D * D, activation_fn=None)
+            loc = slim.fully_connected(h, D, activation_fn=None)
+            scale = slim.fully_connected(h, D, activation_fn=tf.nn.softplus)
+            return loc, scale
+
+        loc, scale = neural_network(self.X_ph)
+        self.Y = ed.models.MultivariateNormalDiag(loc=loc, scale_diag=scale)
+
+        # inference = ed.KLqp(data={self.X_ph: x_train, self.Y: y_train})  # variational inference
+        inference = ed.MAP(data={self.X_ph: x_train, self.Y: y_train})  # MAP approximation
+        inference.run()
+
+        self.sess = ed.get_session()
+
+class EdwardNN0wPrior(EdwardModel):
+
+    def _estimate(self, x_train, y_train):
+        """
+        Parameters
+        ----------
+
+        x_train: NxD array
+            N exemplars of D-dimensional states
+
+        y_train: NxD array
+            N exemplars of D-dimensional successor states
+
+        n_samples: int
+            number of samples to use in inference
+        """
+        N, D = x_train.shape
+
+        # Define the neural network
+        self.X_ph = tf.placeholder(tf.float32, [None, D])
+
+        # define the parameters of the neural network
+        W_0 = ed.models.Normal(loc=tf.zeros([D, D]), scale=tf.ones([D, D]), name="W_0")
+        b_0 = ed.models.Normal(loc=tf.zeros(D), scale=tf.ones(D), name="b_0")
+
+        W_1 = ed.models.Normal(loc=tf.zeros([D, D]), scale=tf.ones([D, D]), name="W_1")
+        b_1 = ed.models.Normal(loc=tf.zeros(D), scale=tf.ones(D), name="b_1")
+
+        # # used for inference
+        qW_0 = ed.models.Normal(loc=tf.Variable(tf.random_normal([D, D]), name="loc"),
+                           scale=tf.nn.softplus(tf.Variable(tf.random_normal([D, D]), name="scale")))
+
+        qb_0 = ed.models.Normal(loc=tf.Variable(tf.random_normal([D])),
+                         scale=tf.nn.softplus(tf.Variable(tf.random_normal([D]))))
+
+        qW_1 = ed.models.Normal(loc=tf.Variable(tf.random_normal([D, D]), name="loc1"),
+                           scale=tf.nn.softplus(tf.Variable(tf.random_normal([D, D]), name="scale1")))
+
+        qb_1 = ed.models.Normal(loc=tf.Variable(tf.random_normal([D])),
+                         scale=tf.nn.softplus(tf.Variable(tf.random_normal([D]))))
+
+        def neural_network(X):
+            """ Neural network with one hidden layer, outputs location and scale parameter
+            for the event model:
+            mean, location = neural_network(X)
+            """
+            loc = tf.matmul(X, W_0) + b_0
+            scale = tf.nn.softplus(tf.matmul(X, W_1) + b_1)
+            return loc, scale
+
+        loc, scale = neural_network(self.X_ph)
+        self.Y = ed.models.MultivariateNormalDiag(loc=loc, scale_diag=scale)
+
+        # inference = ed.KLqp(data={self.X_ph: x_train, self.Y: y_train})  # variational inference
+        inference = ed.KLqp(
+            {W_0: qW_0, b_0: qb_0, W_1: qW_1, b_1: qb_1},
+            data={self.X_ph: x_train, self.Y: y_train}
+        )
+        # inference.run(n_samples=5, n_iter=1000)
+        # inference.run(n_samples=n_samples)
+        # inference = ed.MAP(data={self.X_ph: x_train, self.Y: y_train})  # MAP approximation
+        inference.run()
+
+        self.sess = ed.get_session()
 
 class LinearDynamicSystem(EventModel):
     def __init__(self, D, eta=0.01):
@@ -599,7 +781,7 @@ class SEM(object):
         #
         # # close out any tensorflow sessions
         # for model in event_models.itervalues():
-        #     if hasattr(model, 'close_session'):
+        #     if hasattr(model, 'sess'):
         #         model.close_session()
 
         return post
